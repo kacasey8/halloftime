8f1eb956441badaa2ad21b50522a17c449d2cfc8
Kevin Casey <kacasey@berkeley.edu>
2014-03-09 01:33:43 -0800

    task fixes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -79,6 +79,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:name, :user_id, :project_id, :hours, :minutes)
+      params.require(:task).permit(:name, :user_id, :project_id, :hours, :minutes, :done)
     end
 end
