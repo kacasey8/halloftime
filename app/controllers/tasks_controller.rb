e23d1eb31046f758295d83c8bb836b7e9b2ff614
Kevin Casey <kacasey@berkeley.edu>
2014-03-08 20:27:42 -0800

    feiawlhg

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,7 +25,7 @@ class TasksController < ApplicationController
   # POST /tasks.json
   def create
     @task = Task.new(task_params)
-    @task = current_user.id
+    @task.user = current_user unless @task.user
 
     respond_to do |format|
       if @task.save
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
+
+  validates_presence_of :name, :user_id, :project_id, :hours, :minutes
 end
